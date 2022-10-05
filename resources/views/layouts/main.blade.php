cacff77efa1b1f7618dfe4dbc82402e338041d07
hazman1yusof <hazman.yusof@gmail.com>
2022-10-05 10:50:41 +0800

    sd

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,8 +1,7 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
         <link rel="icon" href="/favicon.ico" type="image/x-icon">
 
         <title>@yield('title')</title>
