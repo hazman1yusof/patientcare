4b16b3698669c018f6f7b53cb455f4e0f516e5a2
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2022-10-26 18:02:39 +0800

    sdsd

--- resources/views/dialysis.blade.php
+++ resources/views/dialysis.blade.php
@@ -181,7 +181,7 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
     <script type="text/javascript" src="{{ asset('assets/printThis.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('js/dialysis_main.js') }}?v=4"></script>
-    <script type="text/javascript" src="{{ asset('js/dialysis.js') }}?v=5"></script>
+    <script type="text/javascript" src="{{ asset('js/dialysis.js') }}?v=6"></script>
     <script type="text/javascript" src="{{ asset('js/patmedication_dialysis.js') }}?v=3"></script>
     <script type="text/javascript" src="{{ asset('js/transaction_dialysis.js') }}?v=3"></script>
 @endsection
