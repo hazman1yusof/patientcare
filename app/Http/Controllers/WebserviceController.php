6b92afcd2e41536c9805ae1fa391842d6e8da0f3
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2023-06-09 12:39:46 +0800

    sdd

--- app/Http/Controllers/WebserviceController.php
+++ app/Http/Controllers/WebserviceController.php
@@ -1396,21 +1396,22 @@ class WebserviceController extends defaultController
                                 ->where('episno',$value_ep->episno)
                                 ->whereMonth('arrival_date','6')
                                 ->min('arrival_date');
+            dump($min_arrival_date);
 
-            $chargetrx_ = DB::table('hisdb.chargetrx')
-                            ->where('compcode','13A')
-                            ->where('chgcode','EP010002')
-                            ->where('mrn',$value_ep->mrn)
-                            ->where('episno',$value_ep->episno)
-                            ->whereMonth('trxdate','6')
-                            ->where('trxdate','<',$min_arrival_date)
-                            ->orderby('trxdate','asc');
+            // $chargetrx_ = DB::table('hisdb.chargetrx')
+            //                 ->where('compcode','13A')
+            //                 ->where('chgcode','EP010002')
+            //                 ->where('mrn',$value_ep->mrn)
+            //                 ->where('episno',$value_ep->episno)
+            //                 ->whereMonth('trxdate','6')
+            //                 ->where('trxdate','<',$min_arrival_date)
+            //                 ->orderby('trxdate','asc');
 
-            if($chargetrx_->exists()){
-                $chargetrx=$chargetrx_->first();
+            // if($chargetrx_->exists()){
+            //     $chargetrx=$chargetrx_->first();
 
-                dump('chgcode:'. $chargetrx->chgcode.' , trxdate:'.$chargetrx->trxdate.' , MRN:'.$chargetrx->mrn.' , Episno:'.$chargetrx->episno.', id:'.$chargetrx->id);
-            }
+            //     dump('chgcode:'. $chargetrx->chgcode.' , trxdate:'.$chargetrx->trxdate.' , MRN:'.$chargetrx->mrn.' , Episno:'.$chargetrx->episno.', id:'.$chargetrx->id);
+            // }
 
             dump('<<<<<>>>>>>');
 
